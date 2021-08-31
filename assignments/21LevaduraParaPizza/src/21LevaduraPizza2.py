697f981cc4db75eb30e5429fd53818e850216a38
EmilioRP1 <89592137+EmilioRP1@users.noreply.github.com>
2021-08-30 23:03:51 -0500

    Add files via upload
    Ejercicio corregido

---
+++ assignments/21LevaduraParaPizza/src/21LevaduraPizza2.py
@@ -0,0 +1,8 @@
+def main():
+    # escribe tu código abajo de esta línea
+    gramos_de_harina = float(input("Dame la harina en gramos: "))
+    gramos_levadura = round(((gramos_de_harina / 1000) * 50),1)
+    print("necesitas estos gramos de levadura:", gramos_levadura)
+
+if __name__ == '__main__':
+    main()
